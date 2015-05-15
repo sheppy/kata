24a84deaaf63e8b25f2ee144bb41c8a49b85c279
Chris Sheppard <chris.sheppard@thebioagency.com>
2015-05-15 16:49:07 +0100

    Adding kata 2 tests

---
+++ tests/kata-02-karate-chop/kata-02-1.spec.js
@@ -0,0 +1,49 @@
+/* global global, describe, it, beforeEach, afterEach */
+/* eslint no-unused-expressions: 0, max-statements: 0 */
+/* jslint -W030, -W071 */
+
+import "../_helpers";
+
+import chop from "../../src/kata-02-karate-chop/kata-02-1.js";
+
+
+describe("chop", () => {
+    it("is a function", () => {
+        chop.should.be.a.func;
+    });
+
+    it("returns an integer", () => {
+        chop().should.return.an.integer;
+    });
+
+
+    const tests = [
+        { needle: 3, haystack: [], result: -1 },
+        { needle: 3, haystack: [1], result: -1 },
+        { needle: 1, haystack: [1], result: 0 },
+
+        { needle: 1, haystack: [1, 3, 5], result: 0 },
+        { needle: 3, haystack: [1, 3, 5], result: 1 },
+        { needle: 5, haystack: [1, 3, 5], result: 2 },
+        { needle: 0, haystack: [1, 3, 5], result: -1 },
+        { needle: 2, haystack: [1, 3, 5], result: -1 },
+        { needle: 4, haystack: [1, 3, 5], result: -1 },
+        { needle: 6, haystack: [1, 3, 5], result: -1 },
+
+        { needle: 1, haystack: [1, 3, 5, 7], result: 0 },
+        { needle: 3, haystack: [1, 3, 5, 7], result: 1 },
+        { needle: 5, haystack: [1, 3, 5, 7], result: 2 },
+        { needle: 7, haystack: [1, 3, 5, 7], result: 3 },
+        { needle: 0, haystack: [1, 3, 5, 7], result: -1 },
+        { needle: 2, haystack: [1, 3, 5, 7], result: -1 },
+        { needle: 4, haystack: [1, 3, 5, 7], result: -1 },
+        { needle: 6, haystack: [1, 3, 5, 7], result: -1 },
+        { needle: 8, haystack: [1, 3, 5, 7], result: -1 }
+    ];
+
+    tests.forEach((test) => {
+        it(`returns ${test.result} when searching [${test.haystack}] for ${test.needle}`, () => {
+            chop(test.needle, test.haystack).should.return(test.result);
+        });
+    });
+});
