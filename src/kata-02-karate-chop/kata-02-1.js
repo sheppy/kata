10b6143b93943733227f69f03a2d7a5d89ad563a
Chris Sheppard <chris.sheppard@thebioagency.com>
2015-05-15 17:24:42 +0100

    Adding kata 2

---
+++ src/kata-02-karate-chop/kata-02-1.js
@@ -0,0 +1,26 @@
+let chop = function (needle, haystack) {
+    let half, result;
+
+    if (!haystack || !haystack.length) {
+        return -1;
+    }
+
+    if (haystack.length === 1) {
+        return (haystack[0] === needle) ? 0 : -1;
+    }
+
+    half = Math.floor(haystack.length / 2);
+    result = chop(needle, haystack.slice(0, half));
+
+    if (result === -1) {
+        result = chop(needle, haystack.slice(half));
+
+        if (result !== -1) {
+            result += half;
+        }
+    }
+
+    return result;
+};
+
+module.exports = chop;
